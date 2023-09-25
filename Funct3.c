88fa9874018b1ccd78813a188e9100c0f148d1dc
Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2023-09-25 12:11:31 +0530

    User Defined Functions

--- Funct3.c
+++ Funct3.c
@@ -2,17 +2,14 @@
 #include <conio.h>
 
 int main(void) {
-	int num1, num2;
-	int my_input(void);   // prototype declaration
-	int my_addition(int, int);
-	// void my_display(int, int, int);
+	int my_input1(void);   // prototype declaration
+	int my_input2(void);   // prototype declaration
+	void my_addition(int, int);
 	void my_goodbye(void);
 	void my_welcome(void);
 	
 	my_welcome();
-	num1 = my_input();
-	num2 = my_input();
-	my_addition(num1, num2);
+	my_addition(my_input1(), my_input2());
 	my_goodbye();
 }
 
@@ -21,22 +18,27 @@ void my_goodbye(void) {
 	printf("\nGood bye and have a nice day...");
 }
 
+void my_display(int n1, int n2, int re) {
+	printf("\n\nSo the sum of %d and %d is %d...", n1, n2, re);
+}
+
 void my_addition(int n1, int n2) {
 	printf("\n\nPerforming the addition operation...");
 	my_display(n1, n2, n1 + n2);
 }
 
-void my_display(int n1, int n2, int re) {
-	printf("\n\nSo the sum of %d and %d is %d...", n1, n2, re);
-}
-
-int my_input(void) {
+int my_input1(void) {
 	int number;
-	printf("\nPlease enter one integer number: ");
+	printf("\nPlease enter the first integer number: ");
 	scanf("%d", &number);
 	return number;
 }
-
+int my_input2(void) {
+	int number;
+	printf("\nPlease enter the second integer number: ");
+	scanf("%d", &number);
+	return number;
+}
 void my_welcome(void) {
 	printf("\nWelcome to the WORLD of C-PROGRAMMING...\n");
 }
