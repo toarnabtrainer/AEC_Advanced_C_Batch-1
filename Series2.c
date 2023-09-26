56fe9ba1b617ac583f6284082a5577d91bd28eea
Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2023-09-26 11:36:36 +0530

    Loop Control

--- Series2.c
+++ Series2.c
@@ -1,7 +1,15 @@
-
-// total = - 1! + 3! - 5! + 7! - 9! + ... n terms
-
+/*
+     i -> 1    2    3    4    5   
+total = - 1! + 3! - 5! + 7! - 9! + ... n terms
+   i   term1   term2
+-----------------------
+   1   2       3   (2 * i), (2 * i + 1)
+   2   4       5
+   3   6       7
+-----------------------
+*/
 #include <stdio.h>
+#include <math.h>
 int main(void) {
 	int n, fact, total, i, j;
 
@@ -11,8 +19,9 @@ int main(void) {
 	total = 0;
 	fact = 1;
 	for(i = 1; i <= n; i++) {
-		???
+		total = total + pow(-1, i) * fact;
 		printf("\nSo the current value of the total = %d and fact = %d...", total, fact);
+		fact = fact * (2 * i) * (2 * i + 1);
 	}
 	
 	printf("\n\nSo the final total is %d...", total);
